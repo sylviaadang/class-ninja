bc775ebac90f2fdc097d801a734c0a44c15baf03
Your Name <email@example.com>
2023-07-03 19:21:44 -0700

    finished

---
+++ ninja.js
@@ -0,0 +1,21 @@
+
+class Ninja {
+    constructor(name, health = 90, speed = 3, strength = 3) {
+        this.name = name
+        this.health = health
+        this.speed = speed
+        this.strength = strength
+    }
+
+    sayName() {
+        console.log(this.name)
+    }
+
+    showStats() {
+        console.log(this.name, this.strength, this.speed, this.health)
+    }
+
+    drinkSake() {
+        this.health++
+    }
+}
